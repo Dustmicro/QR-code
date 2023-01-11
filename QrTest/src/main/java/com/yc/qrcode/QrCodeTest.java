a84ab0efea668ef49470360cad595e8cccb59801
HYF <1648114703@qq.com>
2023-01-11 15:02:08 +0800

    大话降龙之《我的兄弟——顺溜》

--- QrTest/src/main/java/com/yc/qrcode/QrCodeTest.java
+++ QrTest/src/main/java/com/yc/qrcode/QrCodeTest.java
@@ -7,11 +7,11 @@ public class QrCodeTest {
 
     public static void main(String[] args) throws Exception {
         // 存放在二维码中的内容
-        String text = "https://blog.csdn.net/Dustmicro?spm=1000.2115.3001.5343";
+        String text = "https://blog.csdn.net/jingtingfengguo/article/details/51892864";
         // 嵌入二维码的图片路径
-        String imgPath = "D:/图片/3.jpg";
+        String imgPath = "D:/图片/111.jpg";
         // 生成的二维码的路径及名称
-        String destPath = "D:/图片/QR/jam.jpg";
+        String destPath = "D:/图片/QR/Test2.jpg";
         //生成二维码
         QRCodeUtil.encode(text, imgPath, destPath, true);
         // 解析二维码
